4e6a683df9c7aa499bcf616c2d0bb4e4f9de5d8f
Sushant <sushantadhikari70@gmail.com>
2022-08-23 22:12:47 +0545

    changes in comments

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const form=document.getElementById('form')
 const firstname = document.getElementById('firstname')
 const lastname= document.getElementById('lastname')
 const password = document.getElementById('password')
